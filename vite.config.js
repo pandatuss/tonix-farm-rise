e879beb2b7664d91ad933c7e1988da61c8cb453f
pandatuss <imcjjuntilla@gmail.com>
2025-07-23 10:50:55 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,6 @@
-import { defineConfig } from 'vite'
-
-export default defineConfig({
-  // ...other config options
-  server: {
-    allowedHosts: [
-      'serval-generous-secondly.ngrok-free.app'
-      // you can add more allowed hosts here if needed
-    ]
-  }
-})
+server: {
+  allowedHosts: [
+    'serval-generous-secondly.ngrok-free.app'
+    // you can add more allowed hosts here if needed
+  ]
+}
