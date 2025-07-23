edfb50fbd0ee24605b423c6d23cdeef2ddeb316a
pandatuss <imcjjuntilla@gmail.com>
2025-07-23 10:38:36 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,11 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig( ({
   server: {
-    allowedHost:
+    allowedHosts:
   ['serval-generous-secondly.ngrok-free.app'],
-    host: "serval-generous-secondly.ngrok-free.app",
+    host: true,
     port: 8080,
   },
   plugins: [
